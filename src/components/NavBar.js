d7d8f73e69721c2f3cddc2784776d6d8ee4b2916
Karthiga S <karthiga.s@optisolbusiness.com>
2022-10-03 13:21:25 +0530

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,15 +7,7 @@ const NavBar = () => {
         <div className="navbar">
            <i className="fas fa-play"></i>
             <div className="app-header">{metadata.appName}</div>
-            <div className="nav-links">
-                <a
-                    href="https://www.github.com/jessej-samuel/spotipy"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                   
-                </a>
-            </div>
+           
         </div>
     );
 };
